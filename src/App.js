d8e5c81827553afa8bc17f90f92be557ee2ada1d
unknown <sst8872>
2020-01-19 12:00:01 +0900

    todo input added

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import React, {Component} from 'react';
 import uuid from 'uuid';
-import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoInput from "./components/todoInput/TodoInput";
 import TodoList from './components/todoList/TodoList';
 
 class App extends Component {
-   state = {
-       items: [
-           {id: 1, title: "wake up"},
-           {id: 2, title: "make breakfast"}
-       ],
-       id: uuid(),
-       item: '',
-       editItem: false
-   };
-   handleChange = (e) => { console.log('handle change') };
-   handleSubmit = (e) => { console.log('handle Submit') };
-   clearList = () => { console.log('clear list') };
-   handleDelete = (id) => { console.log(`delete ${id}`) };
-   handleEdit = (id) => { console.log(`edit ${id}`) };
-
+    state = {
+        items: [{ id: 1, title: "wake up" }, { id: 2, title: "make breakfast" }],
+        id: uuid(),
+        item: "",
+        editItem: false
+    };
+    handleChange = e => {
+        console.log("handle change");
+    };
+    handleSubmit = e => {
+        console.log("handle Submit");
+    };
+    clearList = () => {
+        console.log("clear list ");
+    };
+    handleDelete = id => {
+        console.log(`handle edit ${id}`);
+    };
+    handleEdit = id => {
+        console.log(`edit edit ${id}`);
+    };
     render() {
         return (
-            <div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-10 mx-auto col-md-8 mt-5">
-                            <h3 className="text-capitalize text-center">Todo Input</h3>
-                            <TodoInput
-                                item={this.state.item}
-                                handleChange={this.handleChange}
-                                handleSubmit={this.handleSubmit}
-                                editItem={this.state.editItem}
-                            />
-                            <TodoList
-                                items={this.state.items}
-                                clearList={this.clearList}
-                                handleDelete={this.handleDelete}
-                                handleEidt={this.handleEdit}
-                            />
-                        </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto col-md-8 mt-5">
+                        <h3 className="text-capitalize text-center">todo input</h3>
+                        <TodoInput
+                            item={this.state.item}
+                            handleChange={this.handleChange}
+                            handleSubmit={this.handleSubmit}
+                            editItem={this.state.editItem}
+                        />
+                        <TodoList
+                            items={this.state.items}
+                            clearList={this.clearList}
+                            handleDelete={this.handleDelete}
+                            handleEdit={this.handleEdit}
+                        />
                     </div>
                 </div>
             </div>
