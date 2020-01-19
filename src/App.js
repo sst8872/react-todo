cf5f4d5c6d38391ee2dc4f54fca4705e4e8a211d
unknown <sst8872>
2020-01-19 11:41:10 +0900

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -6,13 +6,41 @@ import TodoInput from "./components/todoInput/TodoInput";
 import TodoList from './components/todoList/TodoList';
 
 class App extends Component {
+   state = {
+       items: [
+           {id: 1, title: "wake up"},
+           {id: 2, title: "make breakfast"}
+       ],
+       id: uuid(),
+       item: '',
+       editItem: false
+   };
+   handleChange = (e) => { console.log('handle change') };
+   handleSubmit = (e) => { console.log('handle Submit') };
+   clearList = () => { console.log('clear list') };
+   handleDelete = (id) => { console.log(`delete ${id}`) };
+   handleEdit = (id) => { console.log(`edit ${id}`) };
+
     render() {
         return (
             <div>
                 <div className="container">
                     <div className="row">
-                        <TodoInput />
-                        <TodoList />
+                        <div className="col-10 mx-auto col-md-8 mt-5">
+                            <h3 className="text-capitalize text-center">Todo Input</h3>
+                            <TodoInput
+                                item={this.state.item}
+                                handleChange={this.handleChange}
+                                handleSubmit={this.handleSubmit}
+                                editItem={this.state.editItem}
+                            />
+                            <TodoList
+                                items={this.state.items}
+                                clearList={this.clearList}
+                                handleDelete={this.handleDelete}
+                                handleEidt={this.handleEdit}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
